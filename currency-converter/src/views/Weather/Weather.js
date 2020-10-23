a7a3d24087df7fbafd00b7c4dfd1378434e6b7cd
DeskYT <deskytube@gmail.com>
2020-10-23 18:09:13 +0300

    localStorage task released

---
+++ currency-converter/src/views/Weather/Weather.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const HOST = "api.openweathermap.org/data/2.5/";
+const API_KEY = "b8d67052dc6c85fb12d75983c11d464b";
+export default {
+    name: 'Weather',
+    data() {
+        return {
+            students: [],
+
+        }
+    },
+    components: {},
+    methods: {
+
+    }
+
+}
---
+++ currency-converter/src/views/Weather/Weather.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="studentsContainer">
+
+  </div>
+</template>
+<script defer src="@/views/Weather/Weather.js"></script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+}
+</style>
